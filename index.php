e46d279932e746c694e33721a78baf6ef0e5c821
FH-Official <43100028+FH-Official@users.noreply.github.com>
2018-09-08 16:48:01 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+  <head>
+    <title>Stats.Bot - Redirect</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="/styles.css">
+    <script  type="text/javascript" src="/redirect.js"></script>
+    <link rel="shortcut icon" type="image/png" href="https://cdn.glitch.com/2d5360f4-a873-430d-8336-f5f53468f507%2FFNH_PFP.png?1536280408573">
+  </head>
+  <body>
+     <section id="showcase">
+      <div class="container">
+        <h1>Hold tight...</h1>
+        <p>
+          We're currently redirecting you...
+        </p>
+      </div>
+    </section>
+    
+    <?php include 'footer.php'; ?>
+  </body>
+</html>
